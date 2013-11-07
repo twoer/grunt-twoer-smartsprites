dfa8765d9cfedd7071254fb71764245420e740fe
twoer <zhangkun_net@hotmail.com>
2013-11-07 13:43:58 +0800

    init

---
+++ smartsprites-0.2.9/smartsprites.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+rem
+rem Get directory to us
+rem
+set ROOT=%~dp0
+
+rem
+rem Add extra JVM options here
+rem
+set OPTS=-Xms64m -Xmx256m
+
+rem
+rem Build command line arguments
+rem
+set CMD_LINE_ARGS=%1
+if ""%1""=="""" goto doneStart
+shift
+:setupArgs
+if ""%1""=="""" goto doneStart
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+:doneStart
+
+rem
+rem Launch SmartSprites
+rem
+java %OPTS% -Djava.ext.dirs="%ROOT%lib" org.carrot2.labs.smartsprites.SmartSprites %CMD_LINE_ARGS%
+
---
+++ tasks/smartsprite.js
@@ -0,0 +1,57 @@
+/*
+ * grunt-contrib-smartsprite
+ * {%= homepage %}
+ *
+ * Copyright (c) {%= grunt.template.today('yyyy') %} {%= author_name %}
+ * Licensed under the {%= licenses.join(', ') %} license{%= licenses.length === 1 ? '' : 's' %}.
+ */
+
+'use strict';
+
+module.exports = function(grunt) 
+{
+
+    var path = require('path');
+    var cp = require('child_process');
+    var _ = grunt.util._;
+    var f = require('util').format;
+    var log = grunt.log;
+
+    grunt.registerMultiTask('smartsprite', '{%= description %}', function() 
+    {
+        var data = this.data;
+        if(!data.rootPath || !data.outputPath)
+        {
+            log.error('rootPath or outputPath is null.');
+            return;
+        }
+        var stdout = data.stdout !== undefined ? data.stdout : true;
+        var stderr = data.stderr !== undefined ? data.stderr : true;
+        var cmdPath = data.smartspritePath || path.resolve('') + '/node_modules/grunt-contrib-smartsprite/smartsprites-0.2.9/smartsprites.cmd';
+        var rootPath = path.resolve(data.rootPath);
+        var outputPath = path.resolve(data.outputPath);
+        var callback = _.isFunction(data.callback) ? data.callback : function() {}
+        var command = command = cmdPath + ' --root-dir-path "' + rootPath + '" --output-dir-path "' + outputPath + '" --css-file-suffix ""';
+
+        var done = this.async();
+        var childProcess = cp.exec(command, null, callback);
+
+        stdout && childProcess.stdout.on('data', function (data) { log.error('stdout:' + data) });
+        stderr && childProcess.stderr.on('data', function (data) { log.error('stderr:' + data); });
+
+        childProcess.on('exit', function(code) 
+        {
+            if (code !== 0) 
+            {
+                log.error(f('Exited with code: %d.', code));
+                return done(false);
+            }
+            log.ok(f('Exited with code: %d.', code));
+            done();
+        });
+
+    
+    });
+
+
+};
