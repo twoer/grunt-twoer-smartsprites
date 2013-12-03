d47fac0eae206b5b97c86dd3e13e9f9f9b7cc969
twoer <zhangkun_net@hotmail.com>
2013-12-03 14:38:20 +0800

    update to 0.0.7 fix bug

--- tasks/smartsprites.js
+++ tasks/smartsprites.js
@@ -8,42 +8,51 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function(grunt)
+{
 
     var path = require('path');
     var cp = require('child_process');
+    var _ = grunt.util._;
     var f = require('util').format;
     var log = grunt.log;
 
-    grunt.registerMultiTask('smartsprites', '{%= description %}', function() {
-        var options = this.options({
-            stdout: true,
-            stderr: true
-        });
-        if(!options.rootPath || !options.outputPath) {
+    grunt.registerMultiTask('smartsprites', '{%= description %}', function()
+    {
+        var data = this.data;
+        if(!data.rootPath || !data.outputPath)
+        {
             log.error('rootPath or outputPath is null.');
             return;
         }
-        var cmdPath = options.smartspritePath || '"' +  path.resolve('') + '/node_modules/grunt-contrib-smartsprites/smartsprites-0.2.9/smartsprites.cmd"';
-        var rootPath = path.resolve(options.rootPath);
-        var outputPath = path.resolve(options.outputPath);
-        var callback = typeof options.callback === "function" ? options.callback : function() {};
-        var suffix = options.cssFileSuffix || '""';
-        var command = cmdPath + ' --root-dir-path "' + rootPath + '" --output-dir-path "' + outputPath + '" --css-file-suffix ' + suffix;
-
+        var stdout = data.stdout !== undefined ? data.stdout : true;
+        var stderr = data.stderr !== undefined ? data.stderr : true;
+        var cmdPath = data.smartspritePath || '"' +  path.resolve('') + '/node_modules/grunt-contrib-smartsprites/smartsprites-0.2.9/smartsprites.cmd"';
+        var rootPath = path.resolve(data.rootPath);
+        var outputPath = path.resolve(data.outputPath);
+        var callback = _.isFunction(data.callback) ? data.callback : function() {}
+        var suffix = data.cssFileSuffix || '""';
+        var command = command = cmdPath + ' --root-dir-path "' + rootPath + '" --output-dir-path "' + outputPath + '" --css-file-suffix ' + suffix;
+        
         var done = this.async();
         var childProcess = cp.exec(command, null, callback);
 
         stdout && childProcess.stdout.on('data', function (data) { log.error('stdout:' + data) });
         stderr && childProcess.stderr.on('data', function (data) { log.error('stderr:' + data); });
 
-        childProcess.on('exit', function(code) {
-            if (code !== 0) {
+        childProcess.on('exit', function(code)
+        {
+            if (code !== 0)
+            {
                 log.error(f('Exited with code: %d.', code));
                 return done(false);
             }
             log.ok(f('Exited with code: %d.', code));
             done();
         });
+
+
     });
+
+
 };
